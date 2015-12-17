3436402a61ec277a43ce8318f4697fe82a58272a
Ramakrishnan <ramakris@ds.ad.adp.com>
2015-12-17 14:43:17 +0530

    adding the node file

---
+++ index.js
@@ -0,0 +1,32 @@
+const azure = require('azure');
+
+var serviceBusService = azure.createServiceBusService(
+    'Endpoint=sb://nodeasb-ns.servicebus.windows.net/;SharedAccessKeyName=Admin;SharedAccessKey=6nEULsQ0Hsa5SzcFnEZutexg53fiI2d6H1PUMac8jw4='
+    );
+serviceBusService.createQueueIfNotExists('nodeasb', function(error){
+    if(!error){
+        // Queue exists
+        console.log('Queue created sucessfully!');
+    }
+    else {
+        console.log(error);
+    }
+});
+
+
+var message = {
+    body: 'Test message',
+    customProperties: {
+        testproperty: 'TestValue'
+    }};
+serviceBusService.sendQueueMessage('nodeasb', message, function(error){
+    if(!error){
+        // message sent
+        console.log('message setnt sucessfully');
+    }
+    else {
+        console.log(error);
+    }
+});
+
+//Endpoint=sb://nodeasb-ns.servicebus.windows.net/;SharedAccessKeyName=Admin;SharedAccessKey=6nEULsQ0Hsa5SzcFnEZutexg53fiI2d6H1PUMac8jw4=
